c9fadf11914a4c26f4a76fa0bfe94c832b0ac193
wtprogrammer7 <73503565+wtprogrammer7@users.noreply.github.com>
2020-11-13 11:34:52 -0800

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,10 +1,12 @@
-﻿namespace GradeBook.GradeBooks
+﻿using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks //Part2 Step5- Create RankedGradeBook Class: Create a new class RankedGradeBook that inherits the BaseGradeBook class.
 {
     public class RankedGradeBook : BaseGradeBook 
     {
         public RankedGradeBook(string name) : base(name)
         {
-            
+            Type = GradeBookType.Ranked;
         }
 
     }
