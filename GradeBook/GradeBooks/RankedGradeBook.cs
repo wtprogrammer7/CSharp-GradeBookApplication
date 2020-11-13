b1fc7294ff8f94d9130958e9cc443d3a5af5e347
wtprogrammer7 <73503565+wtprogrammer7@users.noreply.github.com>
2020-11-13 12:22:46 -0800

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -14,17 +14,34 @@ namespace GradeBook.GradeBooks //Part2 Step5- Create RankedGradeBook Class: Crea
         public override char GetLetterGrade(double averageGrade)
         {
             if (Students.Count < 5)
-            
+            { 
                 throw new InvalidOperationException("Ranked - grading requires a minimum of 5 students to work");
-            
+            }
 
-            //var threshold = (int)Math.Ceiling(Students.Count * 0.2);
-            //var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+            var threshold = (int)Math.Ceiling(Students.Count * 0.2);
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
 
-            //if(grades)
-            
+            if(grades[threshold-1] <= averageGrade)
+            {
+                return 'A';
+            }
+            else if (grades[threshold*2 - 1] <= averageGrade)
+            {
+                return 'B';
+            }
+            else if (grades[threshold * 3 - 1] <= averageGrade)
+            {
+                return 'C';
+            }
+            else if (grades[threshold * 4 - 1] <= averageGrade)
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'F';
+            }
             //return base.GetLetterGrade(averageGrade);
-            return 'F';
         }
     }
 }
