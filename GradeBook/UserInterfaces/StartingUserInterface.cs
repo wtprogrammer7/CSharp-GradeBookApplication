234c46278ce705778e8578deedbdcb6cbdbb9c15
wtprogrammer7 <73503565+wtprogrammer7@users.noreply.github.com>
2020-11-13 17:33:19 -0800

    testing1

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -41,10 +41,10 @@ namespace GradeBook.UserInterfaces
             }
             var name = parts[1];
             var type = parts[2].ToLower();
-            BaseGradeBook gradeBook = new BaseGradeBook(name);
+            BaseGradeBook gradeBook;
             if (type == "standard")
             {
-                //gradeBook = new StandardGradeBook(name);
+                gradeBook = new StandardGradeBook(name);
             }
             else if (type == "ranked")
             {
